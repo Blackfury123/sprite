67b421f5cfca7ac2228af12ae8a590484bf17cde
Blackfury123 <Shashankreddyp0@gmail.com>
2024-09-26 21:36:54 +0530

    Co-authored-by: Shrey Mishra <shreymishra13@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-from pygame.sprite import _Group
+
 
 pygame.init()
 
@@ -22,13 +22,13 @@ SPRITE_COLOR_CHANGE_EVENT = pygame.USEREVENT + 1
 BACKGROUND_COLOR_CHANGE_EVENT = pygame.USEREVENT + 2
 
 class sprite(pygame.sprite.Sprite):
-    def __init__(self,color , width , height):
+      def __init__(self,color , width , height):
         super().__init__()
         self.image=pygame.Surface([width,height])
         self.image.fill(color)
         self.rect=self.image.get_rect()
         self.velocity=[random.choice([-1,1]),random.choice([-1,1])]
-    def update(self):
+      def update(self):
         self.rect.move_ip(self.velocity)
         boundary_hit=False
 
@@ -43,6 +43,41 @@ class sprite(pygame.sprite.Sprite):
          if boundary_hit:
             pygame.event.post(pygame.event.Event(SPRITE_COLOR_CHANGE_EVENT))
             pygame.event.post(pygame.event.Event(BACKGROUND_COLOR_CHANGE_EVENT))
-    def change_background_color(self):
-       self.image.fill(random.choice([YELLOW,RED,WHITE,LIGHT_BLUE]))    
-        
+      def change_color(self):
+         self.image.fill(random.choice([YELLOW , WHITE,LIGHT_BLUE , RED]))
+
+
+
+def change_background_color():
+      global bg_color
+      bg_color = random.choice([BLUE, BLACK, PINK])
+
+all_sprites_list=pygame.sprite.Group()
+sp1=sprite(WHITE,20,30)
+sp1.rect.x=random.randint(0,450)
+sp1.rect.y=random.randint(0,370) 
+all_sprites_list.add(sp1)
+
+screen=pygame.display.set_mode((600,500))
+pygame.display.set_caption("sprites")
+
+exit=False
+clock=pygame.time.Clock()
+
+while not exit:
+   for event in pygame.event.get():
+      if event.type==pygame.QUIT:
+         exit=True
+      elif event.type==SPRITE_COLOR_CHANGE_EVENT:
+         sp1.change_color()
+      elif event.type == BACKGROUND_COLOR_CHANGE_EVENT:
+         change_background_color()
+
+   all_sprites_list.update()
+   screen.fill(RED)
+   all_sprites_list.draw(screen)
+   pygame.display.flip()
+   clock.tick(240)
+
+
+pygame.quit()
